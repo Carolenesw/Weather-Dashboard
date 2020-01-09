173a004c21b0b316ba97aea91b14f622f66504af
Carolene Wilson <carolenesw@gmail.com>
2020-01-08 21:53:02 -0500

    add varibales, url and api key, plus ajax ger method

--- weather.js
+++ weather.js
@@ -1,5 +1,18 @@
 
-//weather API key 
+$(document).ready(function () {
+  
+var citySearch = $("#city-search");
+var searchArea = $("#search-input");
+var btnSearch = $(".btn");
+var currentCity = $(".current-city");
+var cityName = $(".city");
+var dateN = $(".date"); 
+var windS = $(".wind");
+var tempS = $(".temp");
+var uvIdex = $(".UV");
+var imgN = $(".img");
+
+//API key to run database
  var APIKey = "351b80106cd356a907301219dd0c7806";
 
  // API URL to query database 
@@ -15,3 +28,4 @@
    .then(function(response) {
 
   });
+})
