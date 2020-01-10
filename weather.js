6e0981e30bdb2bc30436e5b945bc5b4b11def53b
Carolene Wilson <carolenesw@gmail.com>
2020-01-10 00:29:01 -0500

    update local storage info and set data to html page

--- weather.js
+++ weather.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
   var humRead = $(".humidity")
   var uvIdex = $(".UV");
   var imgN = $(".img");
-  var cityName = [];
+  var cityArray = [];
   //searchArea = "";
 
   // use moment.js to get date format
@@ -29,52 +29,71 @@ $(document).ready(function () {
 
   //console.log(queryURL);
 
-  // Log the resulting object
+  // set result to HTML page 
+  var storedCity = localStorage.getItem("cityName");
+  cityName.text(storedCity);
+
+  var storedTemp = localStorage.getItem("temperature");
+  tempS.text(storedTemp);
+
+  var storedHumidity = localStorage.getItem("humidity");
+  humRead.text(storedHumidity);
+
+  var storedWind = localStorage.getItem("wind speed");
+  windS.text(storedWind);
+
+
+  //second example
+  // tempS.text(localStorage.getItem("temperature"))
 
   $("#btn").on("click", function (event) {
     event.preventDefault();
 
     // This line get the info from the text area
     searchArea = $("#search-input").val().trim();
+    
+    // console.log(cityName)
 
 
-
-    var userSelection = localStorage.getItem("cityName");
 
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchArea + "&units=imperial&appid=" + APIKey;
     $.ajax({
       url: queryURL,
       method: "GET"
     })
-      // We store all of the retrieved data inside of an object called "response"
+      // We store all of the retrieved data inside of an object called "response" and add required items and variables 
       .then(function (response) {
         console.log(response);
-
-        localStorage.setItem("cityName", searchArea); 
+        //Stored data to local storage 
+        localStorage.setItem("cityName", searchArea + " " + "(" + d + ")");
         // localStorage.setItem("TempS", response.main.temp);
-        localStorage.setItem("Temperature: ", response.wind.speed + " F");
-        localStorage.setItem("Humidity: ", response.main.humidity);
+        localStorage.setItem("temperature", "Temperature: " + response.main.temp + " F");
+        localStorage.setItem("humidity", "Humidity: " + response.main.humidity);
+        // localStorage.setItem("Humidity: ", response.main.humidity);
         // localStorage.setItem("Wind Speed: " + response.wind.speed + " MPH");
-        localStorage.setItem("Wind Speed: ", response.wind.speed + " MPH");
-        
+        localStorage.setItem("wind speed", "Wind-Speed: " + response.wind.speed + " MPH");
 
 
-
+        //show search results on html page  
         $(".city").text(response.name + " " + "(" + d + ")");
         $(".img").text("Icon: " + response.main.icon);
-        
-        // // var temp = localStorage.getItem("Temperature:");
-        // console.log("Temp: "+temp);
-        // $(".temp").text("Temperature:" +temp);
         $(".temp").text("Temperature: " + response.main.temp + " F");
         $(".humidity").text("Humidity: " + response.main.humidity);
         $(".wind").text("Wind Speed: " + response.wind.speed + " MPH");
         $(".UV").text("UV Index: ");
 
+cityName.push(searchArea)
+// $("#search-input").append(searchArea);
 
+
+        // userSelection = response.cityName
+        // for (var i = 0; i < cityName.length; i++) {
+        //   console.log(userSelection);
+
+        // }
       });
 
-    
+
   });
 
 });
