ba3f7ec496e547fb01fcec15c7ece250f6ff2d25
Carolene Wilson <carolenesw@gmail.com>
2020-01-16 00:22:45 -0500

    update five days forecast dates

--- weather.js
+++ weather.js
@@ -78,7 +78,7 @@ $(document).ready(function () {
 
 
     // AJAX call to the run OpenWeatherMap API for 5 days forecast 
-    var queryURLd = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchArea + "&units=imperial&appid=" + APIKey + "&cnt=5";
+    var queryURLd = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchArea + "&units=imperial&appid=" + APIKey;
 
     $.ajax({
       url: queryURLd,
@@ -88,55 +88,65 @@ $(document).ready(function () {
       // // We store all of the retrieved data inside of an object called "data" and and pull each set of data from the following arrays. 
       .then(function (data) {
         console.log(data);
-        console.log(data.list[0]);
-        console.log(data.list[1]);
-        console.log(data.list[2]);
-        console.log(data.list[3]);
-        console.log(data.list[4]);
+        // console.log(data.list[0]);
+        // console.log(data.list[1]);
+        // console.log(data.list[2]);
+        // console.log(data.list[3]);
+        // console.log(data.list[4]);
 
         var dayOne = $(".badge1");
         var dayTwo = $(".badge2");
 
         //create future date for forecast
-        var dayAhead = moment().format('L');
-        $(".badge1").append(dayAhead);
-        $(".badge2").append(dayAhead);
-        $(".badge3").append(dayAhead);
-        $(".badge4").append(dayAhead);
-        $(".badge5").append(dayAhead);
+        // var dayAhead = moment().format('L');
+        // $(".badge1").append(dayAhead);
+        // $(".badge2").append(dayAhead);
+        // $(".badge3").append(dayAhead);
+        // $(".badge4").append(dayAhead);
+        // $(".badge5").append(dayAhead);
 
         //set five day forcast data to local storage 
         //day one data 
         localStorage.setItem("temp", "Temp: " + data.list[0].main.temp + "F" + "");
         localStorage.setItem("humid", "humid: " + data.list[0].main.humidity + "%");
+        localStorage.setItem("time", "Time: " + data.list[0].dt_txt);
         //day two data
-        localStorage.setItem("temp1", "Temp1: " + data.list[1].main.temp + "F" + "");
-        localStorage.setItem("humid1", "humid1: " + data.list[1].main.humidity + "%");
+        localStorage.setItem("temp1", "Temp1: " + data.list[6].main.temp + "F" + "");
+        localStorage.setItem("humid1", "humid1: " + data.list[6].main.humidity + "%");
+        localStorage.setItem("time", "Time: " + data.list[6].dt_txt);
         //day three data 
-        localStorage.setItem("temp2", "Temp2: " + data.list[2].main.temp + "F" + "");
-        localStorage.setItem("humid2", "humid2: " + data.list[2].main.humidity + "%");
+        localStorage.setItem("temp2", "Temp2: " + data.list[12].main.temp + "F" + "");
+        localStorage.setItem("humid2", "humid2: " + data.list[12].main.humidity + "%");
+        localStorage.setItem("time", "Time: " + data.list[12].dt_txt);
         //day four data 
-        localStorage.setItem("temp3", "Temp3: " + data.list[3].main.temp + "F" + "");
-        localStorage.setItem("humid3", "humid3: " + data.list[3].main.humidity + "%");
+        localStorage.setItem("temp3", "Temp3: " + data.list[18].main.temp + "F" + "");
+        localStorage.setItem("humid3", "humid3: " + data.list[18].main.humidity + "%");
+        localStorage.setItem("time", "Time: " + data.list[18].dt_txt);
         //day five data 
-        localStorage.setItem("temp4", "Temp4: " + data.list[4].main.temp + "F" + "");
-        localStorage.setItem("humid4", "humid4: " + data.list[4].main.humidity + "%");
+        localStorage.setItem("temp4", "Temp4: " + data.list[24].main.temp + "F" + "");
+        localStorage.setItem("humid4", "humid4: " + data.list[24].main.humidity + "%");
+        localStorage.setItem("time", "Time: " + data.list[24].dt_txt);
 
         //append data from local storage to respective badge 
         $(".badge1").append("Temp: " + data.list[0].main.temp + "F" + "");
         $(".badge1").append("Humid: " + data.list[0].main.humidity + "%");
+        $(".badge1").append("Time: " + data.list[0].dt_txt);
 
-        $(".badge2").append("Temp: " + data.list[1].main.temp + "F" + "");
-        $(".badge2").append("Humid: " + data.list[1].main.humidity + "%");
+        $(".badge2").append("Temp: " + data.list[6].main.temp + "F" + "");
+        $(".badge2").append("Humid: " + data.list[6].main.humidity + "%");
+        $(".badge2").append("Time: " + data.list[6].dt_txt);
 
-        $(".badge3").append("Temp: " + data.list[2].main.temp + "F" + "");
-        $(".badge3").append("Humid: " + data.list[2].main.humidity + "%");
+        $(".badge3").append("Temp: " + data.list[14].main.temp + "F" + "");
+        $(".badge3").append("Humid: " + data.list[14].main.humidity + "%");
+        $(".badge3").append("Time: " + data.list[14].dt_txt);
 
-        $(".badge4").append("Temp: " + data.list[3].main.temp + "F" + "");
-        $(".badge4").append("Humid: " + data.list[3].main.humidity + "%");
+        $(".badge4").append("Temp: " + data.list[22].main.temp + "F" + "");
+        $(".badge4").append("Humid: " + data.list[22].main.humidity + "%");
+        $(".badge4").append("Time: " + data.list[22].dt_txt);
 
-        $(".badge5").append("Temp: " + data.list[4].main.temp + "F " + "");
-        $(".badge5").append("Humid: " + data.list[4].main.humidity + "%");
+        $(".badge5").append("Temp: " + data.list[30].main.temp + "F " + "");
+        $(".badge5").append("Humid: " + data.list[30].main.humidity + "%");
+        $(".badge5").append("Time: " + data.list[30].dt_txt);
 
 
       });
